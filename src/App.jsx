237c24cbc17804241f1d4ae18ae03c04ca10d773
shahin hossain <shahinhossain3@hotmail.com>
2023-12-19 23:38:55 +0600

    Create React form and Post API and send data to the server

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,49 @@ function App() {
       .then(data => setUsers(data))
   }, [])
 
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const from = event.target;
+    const name = from.name.value;
+    const email = from.email.value;
+
+    const user = { name, email }
+
+    console.log(user)
+    // server এ post request পাঠানোর জন্য নিচের পদ্ধিতি ব্যবহার করা হয়।
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST', // method বলে দেয়া হয়েছে, POST method ব্যবহার করা হয় ‍server এ post করার জন্য ।
+
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user), // body এর মধ্যে object কে json stringify করে পাঠানো হয়।
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+
+  }
+
   return (
     <>
       <h1>User Management Systems</h1>
       <h3>Numbers of Users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='email' />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
 
-
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.id}. {user.name} {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
