3eb45caea047e9e582abe9184b4671008f15e60c
shahin hossain <shahinhossain3@hotmail.com>
2023-12-20 20:11:26 +0600

    Send Client data to the server and display data on the client

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,14 @@ function App() {
       method: 'POST', // method বলে দেয়া হয়েছে, POST method ব্যবহার করা হয় ‍server এ post করার জন্য ।
 
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json' //কি ধরনের data যাবে backend এ তা বলে দেয়া হয়েছে header এর মধ্যে।
       },
       body: JSON.stringify(user), // body এর মধ্যে object কে json stringify করে পাঠানো হয়।
     })
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        const newUser = [...users, data]; // আগের user এবং নিউ user
+        const newUser = [...users, data]; // আগের user এবং নিউ user কে একটা array এর মধ্যে রাখা হয়েছে।
         setUsers(newUser)
         from.reset()
       })
